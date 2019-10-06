57c7b71126791a2c77bf880ee4a420de12bdcf17
yukinagano <tawapeee16@gmail.com>
2019-10-05 23:35:15 -0700

    changed text

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,28 +3,21 @@ var router = express.Router();
 const mongoose = require('mongoose');
 const Message = mongoose.model('Message');
 
-// 変更0916
+// -----------
 const app = express();
 const cors = require('cors');
 
 app.use(cors());
 //-------------
 
-// app.get('/test', (req, res) => {
-//     res.send('hi')
-// });
-
 router.get('/', (req, res) => {
     res.render("message/addOrEdit",{
-        viewTitle : "Edit Message"
+        viewTitle : "New Message"
     });    
 });
 
 router.post('/', (req, res) => {
-    // if (req.body._id == '')
         insertRecord(req, res);
-    //     else
-        // updateRecord(req, res);
 });
 
 mongoose.set('useFindAndModify', false);
@@ -40,21 +33,13 @@ function insertRecord(req, res) {
         if (!err)
             res.redirect('message/list');
         else {
-            // if (err.name == 'ValidationError'){
-            //     handleValidationError(err, req.body);
-            //     res.render("employee/addOrEdit",{
-            //         viewTitle : "Insert Employee",
-            //         employee: req.body
-            //     });
-            // }
-            // else
                 console.log('Error during record insection : ' + err);
         }
     });
 }
 
 function updateRecord(req, res) {
-    // Employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+    // Message.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
     Message.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err) => {
     
     if (!err) { res.redirect('message/list'); }
@@ -76,21 +61,6 @@ router.get('/list', (req, res) => {
         }
     })
 });
-
-// function handleValidationError(err, body) {
-//     for (field in err.errors) {
-//         switch (err.errors[field].path) {
-//             case 'fullName':
-//                 body['fullNameError'] = err.errors[field].message;
-//                 break;
-//             case 'email':
-//                 body['emailError'] = err.errors[field].message;
-//                 break;
-//             default:
-//                 break;
-//         }
-//     }
-// }
 
 router.get('/:id', (req, res) => {
     Message.findById(req.params.id, (err, doc) => {
